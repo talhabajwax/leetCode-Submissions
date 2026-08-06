2162ca17a6a96d15a5f2b88898baeb0985184e55
Talha Ansar <93472275+talhabajwax@users.noreply.github.com>
2026-08-06 06:21:24 +0500

    Time: 0 ms (100%), Space: 19.3 MB (28.49%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -0,0 +1,15 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        number=[1]
+        rows=[]
+        while len(rows) <numRows:
+            rows.append(number)
+            temp=[1]
+            for i in range(1,len(number)):
+                temp.append(number[i - 1] + number[i])
+            temp.append(1)
+            number=temp
+        return rows                
+
+
+                    
