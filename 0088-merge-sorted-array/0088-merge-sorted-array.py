0be6bc0dc6a9100cce684e3d4f8c800430b5a0f6
Talha Ansar <93472275+talhabajwax@users.noreply.github.com>
2026-09-20 04:33:20 +0500

    Time: 0 ms (100%), Space: 19.4 MB (40.05%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,30 +1,15 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
-        num1_index=(m-1)
-        num2_index=(n-1)
-        num1_last_index= n+m-1
-        while num2_index>=0:
-            if num1_index>=0:
-                if nums1[num1_index] < nums2[num2_index]:
-                    nums1[num1_last_index] = nums2[num2_index]
-                    num2_index = num2_index-1
-                    num1_last_index = num1_last_index-1
-                 
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
 
-                elif nums1[num1_index] > nums2[num2_index]:
-                     nums1[num1_last_index] =  nums1[num1_index]
-                     num1_index = num1_index-1 
-                     num1_last_index = num1_last_index-1
-                elif nums1[num1_index] == nums2[num2_index]:
-                     nums1[num1_last_index] = nums2[num2_index]
-                     num2_index = num2_index-1
-                     num1_last_index = num1_last_index-1
+        while j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
             else:
-                        nums1[num1_last_index] = nums2[num2_index]
-                        num2_index = num2_index-1
-                        num1_last_index = num1_last_index-1
+                nums1[k] = nums2[j]
+                j -= 1
 
-                     
+            k -= 1
